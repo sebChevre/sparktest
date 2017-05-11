97dad2ad25b1fe6e1f2b78fd86db3401b86ade5c
seb <seb.chevre>
2017-05-11 21:37:00 +0200

    Server port 8080 and route /

--- src/main/java/ch/sebooom/openshift/sparktest/Server.java
+++ src/main/java/ch/sebooom/openshift/sparktest/Server.java
@@ -1,6 +1,7 @@
 package ch.sebooom.openshift.sparktest;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 /**
  * Created by seb on .
@@ -10,7 +11,14 @@ import static spark.Spark.get;
 public class Server {
 
     public static void main(String[] args) {
+
+        port(8080);
+
+        get("/", (req, res) -> "maingit add .");
+
         get("/hello", (req, res) -> "Hello World");
+
+        get("/hello2", (req, res) -> "Hello World");
 
     }
 }
